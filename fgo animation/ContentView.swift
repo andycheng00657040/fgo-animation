d4f6bbc8fe1de4f2dbd4777a33258a96c15a03c0
andycheng <andychz0519@gmail.com>
2020-04-14 13:02:26 +0800

    Initial Commit

---
+++ fgo animation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  fgo animation
+//
+//  Created by User12 on 2020/4/14.
+//  Copyright © 2020 hunteruf. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
